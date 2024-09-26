9248d6a78e87c9b05438e1a25801965c21ea2dbd
Vanesa <vanegoho@gmail.com>
2024-09-26 10:52:10 -0600

    Carga de archivos

--- src/unidad1/prolineal/GaussR.java
+++ src/unidad1/prolineal/GaussR.java
@@ -1,44 +1,61 @@
-package unidad1.proglineal;
+package unidad1.prolineal;
 
+/**
+ * para evitar errores generador de codigo
+ *
+ * @author vanesahonorato
+ */
 public class GaussR {
 
-    public static double[][] simplex(double[][] A, int R, int C) {
-        int numRows = A.length;
-        int numCols = A[0].length;
+    private int c;
+    private int r;
+    private double[][] pl;
 
-        // Comprobamos si el pivote es 0
-        if (A[R][C] == 0) {
-            System.out.println("-1");
-            return null;  // Salida -1 si el pivote es cero.
-        }
-
-        // Hacer 1 el elemento pivote dividiendo el renglón R por el elemento en A[R][C]
-        double pivot = A[R][C];
-        for (int j = 0; j < numCols; j++) {
-            A[R][j] /= pivot;
-        }
-
-        // Hacer 0 los demás elementos de la columna C
-        for (int i = 0; i < numRows; i++) {
-            if (i != R) {  // No tocar el renglón pivote
-                double factor = A[i][C];
-                for (int j = 0; j < numCols; j++) {
-                    A[i][j] -= factor * A[R][j];  // Restar un múltiplo del renglón R
-                }
-            }
-        }
-
-        return A;
+    public GaussR(int c, int r, double[][] pl) {
+        this.c = c;
+        this.r = r;
+        this.pl = pl;
     }
 
-    // Método para imprimir la matriz
-    public static void printMatrix(double[][] A) {
-        for (double[] row : A) {
-            for (double value : row) {
-                System.out.printf("%8.4f", value);
-            }
-            System.out.println();
-        }
+   
+    public double[][] getPl() {
+        return pl;
     }
 
+    public int getR() {
+        return r;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void pivoting() {
+        double piv = pl[r][c];
+        for (int j = 1; j < pl[0].length; j++) 
+            pl[r][j] = pl[r][j] / piv;
+        
+        
+        
+        for (int i = 0; i <pl.length; i++) {
+            if (i != r){
+                 double fact =-pl[i][c]; 
+
+                for (int j = 0; j < pl[0].length; j++) 
+                                  
+                pl[i][j]= pl[r][j]* fact + pl[i][j];
+                
+             }
+        } 
+    }
+
+    public void print() {
+        System.out.println("pl:--------------------------------");
+        for (int i = 0; i < pl.length; i++) {
+            for (int j = 0; j < pl[0].length; j++) {
+                System.out.printf("%+6.3f ", pl[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 }
