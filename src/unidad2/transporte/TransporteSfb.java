cc77051c685943c000b173787bf66ff98fd6af14
Vanesa <vanegoho@gmail.com>
2024-10-30 19:59:43 -0600

    Implementación del método de la esquina noroeste y pruebas

---
+++ src/unidad2/transporte/TransporteSfb.java
@@ -0,0 +1,10 @@
+
+package unidad2.transporte;
+
+/**
+ *
+ * @author vanesahonorato
+ */
+public abstract class TransporteSfb {
+     public abstract void sfb(int[][] mn, int[] d, int[] o);
+}
---
+++ src/unidad2/transporte/TransporteSfbEN.java
@@ -0,0 +1,21 @@
+package unidad2.transporte;
+
+public class TransporteSfbEN extends TransporteSfb {
+
+    @Override
+    public void sfb(int[][] mn, int[] d, int[] o) {
+        int r = mn.length;
+        int c = mn[0].length;
+        int i = 0, j = 0;
+
+        while (i < r && j < c) {
+            int a = Math.min(o[i], d[j]);
+            mn[i][j] = a;
+            o[i] -= a;
+            d[j] -= a;
+
+            if (o[i] == 0) i++;
+            if (d[+j] == 0) j++;
+        }
+    }
+}
